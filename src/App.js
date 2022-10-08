5ff124119e55e17329a44f1f59caa2fac47c5e7b
Abdur R. Walid <ar_olid@outlook.com>
2022-10-07 22:30:41 -0600

    fixed a file name

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import "./App.css";
-import Header from "./Components/Header/Header";
+import NavBar from "./Components/NavBar/NavBar";
 
 function App() {
   return (
     <div className="App">
-      <Header />
+      <NavBar />
     </div>
   );
 }
