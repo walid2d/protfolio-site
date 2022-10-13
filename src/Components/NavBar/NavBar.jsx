7e6d27a9095b20455cb47bf2dc61e876d659b637
Abdur R. Walid <ar_olid@outlook.com>
2022-10-12 23:12:50 -0600

    added a two logos and also the changed the design of the pfp

--- src/Components/NavBar/NavBar.jsx
+++ src/Components/NavBar/NavBar.jsx
@@ -1,19 +1,19 @@
 import React from "react";
-import ProtLogo from "../../Assets/ProtLogo.png";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faMoon, faSun } from "@fortawesome/free-solid-svg-icons";
+import MainLogo from "../../Assets/MainLogo.png";
+import NightMode from "../../Assets/NightMode.png";
 
 function NavBar() {
   return (
     <header className="flex flex-row justify-between">
-      <img className="w-12 h-12 animate-spin" alt="" src={ProtLogo}></img>
+      <img className="w-14 h-14" alt="" src={MainLogo}></img>
       <div className="flex justify-between flex-row items-center space-x-1.5">
         <ul className="flex justify-between flex-row items-center space-x-9 mr-2">
           <li className="text-base text-white ">About</li>
           <li className="text-base text-white">Toolbox</li>
           <li className="text-base text-white">Contact</li>
         </ul>
-        <FontAwesomeIcon icon={faSun} className="text-white text-base" />
+
+        <img src={NightMode} alt="" className="w-14 h-14" />
       </div>
     </header>
   );
